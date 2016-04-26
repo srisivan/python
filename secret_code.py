6a81e302c0f0c4792bee77c51f5cd5784be98442
Srisivan <srisivan.k@gmail.com>
2016-04-26 20:19:35 +0530

    Program to check magic string

---
+++ secret_code.py
@@ -0,0 +1,20 @@
+
+Secret_bank_code = "12G4K6D7N3"
+
+code = input("Enter the code, please\n")
+
+if (code == Secret_bank_code):
+	print("Your bank account can be accessed.You can go in\n")
+
+Hacking = "http://secret_bank_codes.com"
+
+print(Hacking)
+print(code)
+
+if (code == Hacking):
+	print("ERROR! ERROR! ERROR!\n")
+	print("YOU'RE A HACKER! YOU'RE TRYING TO HACK THE BANK ACCOUNT\n")
+else:
+	print("YOU'RE NOT THE CORRECT PERSON!YOU CANNOT GO IN!\n")
+
+
