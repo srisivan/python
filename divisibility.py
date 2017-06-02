c8babd519a02ae4cc7c7babf41e500742b92b443
Srisivan <srisivan.k@gmail.com>
2017-06-02 20:24:53 +0530

    improved version

--- divisibility.py
+++ divisibility.py
@@ -2,14 +2,16 @@
 # To find divisible numbers for 3.
 
 divisors = []
-my_num = 3
+my_num = int(input("Enter the desired number : "))
 
-for i in range (1, 100):
+for i in range (1, (my_num + 1)):
     if my_num % i == 0:
         divisors.append(i)
         length = len(divisors)
-        if length == 2:
-            print(" %s is a prime" % my_num)
+
+if length == 2:
+    print("%s is a prime number" % my_num)
+else:
+    print("%s is a composite number" % my_num)
 
 print(divisors)
-
