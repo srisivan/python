da580f7557e0220bdb35e8dd2f655bb45a859d43
Srisivan <srisivan.k@gmail.com>
2017-09-24 09:17:20 +0530

    The program to calculate the salary for a company.

--- salary.py
+++ salary.py
@@ -15,11 +15,15 @@ FPF = (Basic + DA) - (3.67 * 100)      # On Basic + DA
 Salary_advance = 2500
 Tex_Loan = 3000
 
+# Total earnings for the employee
 Total_Earnings = PF + FPF + HRA + CA + WA
 
+# Deductions to be detected from the toatal earnings
 Total_Deductions = PF + FPF + Salary_advance + Tex_Loan
 
+# Final Salary for employee, after subtracting
 total_salary = Total_Earnings - Total_Deductions
+
 
 print("Total Earnings = %s " % Total_Earnings)
 print("Total Deductions = %s " % Total_Deductions)
@@ -27,11 +31,16 @@ print("Final_salary = %s " % total_salary)
 print("PF = %s " % PF)
 print("FPF = %s" % FPF)
 
+# Salary for one day.
 one_day_salary = (total_salary / 30) 
 
+# Number of days the employee was absent.
 No_of_days_absent = int(input("Enter the number of days your employee was absent : "))
+
+# No of days the employee was present
 No_of_days_present = 30 - No_of_days_absent
 
+# Salary for the days the employee was present.
 Second_salary = one_day_salary * No_of_days_present
 
 print("Salary for one day = %s" % one_day_salary)
